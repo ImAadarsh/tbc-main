33eaefcd827198be4e67600e64675d88ce32eb9e
Suryapratap Singh Suryavanshi <suryprtaps@gmail.com>
2023-07-14 01:51:12 +0530

    Fix: api key issue [if hide]

--- pages/api/geoapi.js
+++ pages/api/geoapi.js
@@ -5,5 +5,8 @@ export default async function handler(req, res) {
         `https://maps.googleapis.com/maps/api/place/queryautocomplete/json?input=${req.query.text}&key=${API_KEY}`,
     ).then(response => response.json());
     
-    res.json(data);
+    res.json({
+        key: API_KEY,
+        data: data
+    });
 }
