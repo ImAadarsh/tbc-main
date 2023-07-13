db73adfe628dd7fdf79e387e60dac13658b09f67
Suryapratap Singh Suryavanshi <suryprtaps@gmail.com>
2023-07-14 02:03:41 +0530

    Fix: expose api by next_public_

--- pages/api/geoapi.js
+++ pages/api/geoapi.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    const API_KEY = process.env.GOOGLE_API_KEY;
+    const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
 
     const data = await fetch(
         `https://maps.googleapis.com/maps/api/place/queryautocomplete/json?input=${req.query.text}&key=${API_KEY}`,
