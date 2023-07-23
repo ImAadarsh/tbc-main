87005b7e70f1e15f4d08b3a39f6ee2448f4a472e
Suryapratap Singh Suryavanshi <suryprtaps@gmail>
2023-07-24 02:43:10 +0530

    Add soon

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-        <>
-            <a href="/ambassadors">ambassadors</a>
-        </>
+        <div style={{display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", width: "100%"}}>
+            <h1>Launching Soon</h1>
+        </div>
     );
 }
