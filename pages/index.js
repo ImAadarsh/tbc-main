c74cb15008c972c56049938cc99a4f3eb35fe068
Aadarsh Gupta <67497622+ImAadarsh@users.noreply.github.com>
2023-07-27 17:21:27 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Link from "next/link"
 export default function Home() {
     return (
         <div style={{display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", width: "100%"}}>
+                                    <img src="/Logo_Full.svg" alt="logo" />
             <h1>Launching Soon</h1>
+    <Link href={'/ambassadors'}>
+                  <button onClick={() => {
+                                handelLogin()
+                            }} className="primary_button">Amabassador Login</button>
+                    </Link>
+    
         </div>
     );
 }
