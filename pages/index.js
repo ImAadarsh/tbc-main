f1776929218833ed9026ca96a550039d65579b48
Aadarsh Gupta <67497622+ImAadarsh@users.noreply.github.com>
2023-08-05 23:59:25 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import Link from "next/link"
 export default function Home() {
     return (
-        <div style={{display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", alignContent: "space-around", flexWrap: "wrap", flexDirection: "column" }}>
-                                    <img src="/Logo_Full.svg" alt="logo" /><br/>
-            <h1>Launching Soon</h1><br/>
-    <Link href={'/ambassadors'}>
-                  <button onClick={() => {
-                                handelLogin()
-                            }} className="primary_button">Amabassador Login</button>
-                    </Link>
-    
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            minHeight: "100vh", // Changed height to minHeight for responsiveness
+            alignContent: "space-around",
+            flexWrap: "wrap",
+            flexDirection: "column",
+            padding: "20px" // Added padding for better spacing on smaller screens
+        }}>
+            <img src="/Logo_Full.svg" alt="logo" style={{ maxWidth: "100%", marginBottom: "20px" }} /><br/>
+            <h1 style={{ fontSize: "24px", textAlign: "center", marginBottom: "20px" }}>Launching Soon</h1><br/>
+            <Link href={'/ambassadors'}>
+                <button onClick={() => {
+                    handelLogin()
+                }} className="primary_button" style={{ width: "85%" }}>Amabassador Login</button>
+            </Link>
         </div>
     );
 }
