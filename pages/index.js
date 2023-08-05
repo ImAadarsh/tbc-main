cefa019988dfc42755d86de47d66efd238c689fb
Aadarsh Gupta <67497622+ImAadarsh@users.noreply.github.com>
2023-08-06 00:04:42 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,14 @@ export default function Home() {
             alignContent: "space-around",
             flexWrap: "wrap",
             flexDirection: "column",
-            padding: "20px" // Added padding for better spacing on smaller screens
+            padding: "5px" // Added padding for better spacing on smaller screens
         }}>
             <img src="/Logo_Full.svg" alt="logo" style={{ maxWidth: "100%", marginBottom: "10px" }} /><br/>
             <h1 style={{ fontSize: "24px", textAlign: "center", marginBottom: "10px" }}>Launching Soon</h1><br/>
             <Link href={'/ambassadors'}>
                 <button onClick={() => {
                     handelLogin()
-                }} className="primary_button" style={{ width: "100%", fontSize: "14px", fontStyle: "none" }}>Amabassador Login</button>
+                }} className="primary_button" style={{ width: "100%", fontSize: "14px", fontStyle: "none", paddingLeft: "15px", paddingRight: "15px" }}>Amabassador Login</button>
             </Link>
         </div>
     );
